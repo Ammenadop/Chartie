b959e28bc34792f6b06a103aadaecc92138d0c59
Ammenadop <kiftkinz49@gmail.com>
2023-07-31 00:21:11 +0530

    many changes

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken ? req.cookies.accessToken : req.headers;
+  const token = req.cookies.accessToken ? req.cookies.accessToken : req.headers.token;
   if (!token) {
     return res.status(401).json({
       success: false,
